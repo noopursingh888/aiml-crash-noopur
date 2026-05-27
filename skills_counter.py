79ef473739597ed7ca9711872b71aa5e9e4b3797
Noopur Singh <noopursingh775@gmail.com>
2026-05-28 02:48:34 +0530

    Q2: skills counter

---
+++ skills_counter.py
@@ -0,0 +1,6 @@
+skills = ["Python", "SQL", "volleyball", "Communication", "Github"]
+
+for index, skill in enumerate(skills, start=1):
+    print(f"{index}. {skill}")
+
+print(f"Total skills: {len(skills)}")
